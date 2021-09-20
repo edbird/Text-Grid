aad566b0d0284398dc6b9a5e02001c22296f32c0
Edward Birdsall <edward.birdsall@postgrad.manchester.ac.uk>
2021-09-20 22:45:55 +0100

    put filltextgridfromtextbuffer function in new file, added a comment about problem with textgrid constructor logic, re-enabled filltextgridfromtextbuffer function in main

--- main.cpp
+++ main.cpp
@@ -35,97 +35,7 @@
 
 
 
-void fillTextGridFromTextBuffer(TextGrid& textgrid, TextBuffer &textbuffer)
-{
 
-    // TODO: need to clear the line by writing " " characters
-    // if the substring is shorter than size_x
-
-    unsigned int size_x{textgrid.m_size_x};
-    unsigned int size_y{textgrid.m_size_y};
-
-    std::size_t scroll_pos = textbuffer.m_scroll;
-    unsigned int u_scroll_pos = (unsigned int)scroll_pos;
-    bool wrap = textbuffer.m_wrap;
-
-    if(wrap)
-    {
-        std::size_t textbuffer_wrap_line_count(0);
-        //std::size_t textbuffer_line_index(0);
-
-        for(std::size_t ix{0}; ix < textbuffer.m_lines.size(); ++ ix)
-        {
-            // line index is ix
-            std::size_t line_length{textbuffer.m_lines.at(ix).size()};
-
-            /*
-            // number of lines after wrapping
-            unsigned int number_of_lines =
-                ((unsigned int)line_length + size_x - 1) / size_x;
-            */
-
-
-            // new approach, iterate through line with increment of size_x
-
-            for(unsigned int i{0}; i < line_length; i += size_x)
-            {
-                if((textbuffer_wrap_line_count <= scroll_pos) &&
-                    (textbuffer_wrap_line_count < scroll_pos + size_y))
-                {
-                    unsigned int delta = textbuffer_wrap_line_count - scroll_pos;
-                    std::cout << "delta=" << delta << std::endl;
-
-                    std::string substring = textbuffer.m_lines.at(ix).substr(i, size_x);
-
-                    std::cout << "substring=" << substring << std::endl;
-
-                    textgrid.PutString(0, delta, substring); // out of bounds ok
-
-                    ++ textbuffer_wrap_line_count;
-                }
-            }
-
-            // efficient: quit early once textgrid filled
-            if(textbuffer_wrap_line_count >= size_y) break;
-        } // for
-
-
-    }
-    else
-    {
-        // TODO: substr() will throw error if pos > size !
-
-        // no wrap version
-        std::size_t textbuffer_line_count(0);
-
-        std::size_t scroll_pos_h = textbuffer.m_scroll_h;
-
-        for(std::size_t ix{0}; ix < textbuffer.m_lines.size(); ++ ix)
-        {
-            // line index is ix
-            std::size_t line_length{textbuffer.m_lines.at(ix).size()};
-
-            // no comment here
-
-            if((textbuffer_line_count <= scroll_pos) &&
-                (textbuffer_line_count < scroll_pos + size_y))
-            {
-                unsigned int delta = textbuffer_line_count - scroll_pos;
-        
-                std::string substring = textbuffer.m_lines.at(ix).substr(scroll_pos_h, size_x);
-
-                textgrid.PutString(0, delta, substring); // out of bounds ok
-
-                ++ textbuffer_line_count;
-            }
-
-            // efficient: quit early once textgrid filled
-            if(textbuffer_line_count >= size_y) break;
-        } // for
-
-    } // else
-
-}
 
 
 int main(int argc, char *argv[])
@@ -206,15 +116,25 @@ int main(int argc, char *argv[])
 
     // TODO: this constructor call makes the SetFont function
     // redundant
-    TextGrid textgrid(10, 10, font_texture_liberation_mono);
+    TextGrid textgrid(40, 20, font_texture_liberation_mono);
     //textgrid.SetFont(font_texture_liberation_mono);
 
+    // here is a potential problem:
+    // the textgrid size is set assuming fixed width / monospace font
+    // the arguments are the number of characters in x and y
+    // whereas in reality the arguments should be the pixel size of
+    // the drawable textgrid area
+    // otherwise, when using different font sizes or non-monospace fonts
+    // the drawable area of the textgrid scales (changes) rather than
+    // remaining static
+
     // TODO: removed this to test base functions
-    //fillTextGridFromTextBuffer(textgrid, textbuffer);
+    fillTextGridFromTextBuffer(textgrid, textbuffer);
     
 
     // TODO: need to change the X and Y around so the indices
     // are row, column
+    /*
     textgrid.Put(0, 0, '0');
     textgrid.Put(1, 0, '1');
     textgrid.Put(2, 0, '2');
@@ -227,7 +147,7 @@ int main(int argc, char *argv[])
 
     textgrid.Put(4, 4, 'X');
     textgrid.PutString(0, 4, "sho");
-
+    */
 
     /*
     // find a font file
